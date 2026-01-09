c5c46781358ee4da111788f6abc082274c633ec6
3smithgo <61187066+3smithgo@users.noreply.github.com>
2026-01-09 12:07:05 -0800

    Add hamsterimg.net as an alternate to hamster.is per notice (#627)
    * Add hamsterimg.net as an alternate to hamster.is per notice
    
    * add actual logic
    
    ---------
    
    Co-authored-by: Owyn <Owyn@users.noreply.github.com>

--- HandyImage.user.js
+++ HandyImage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name		Handy Image
-// @version		2025.12.07
+// @version		2026.01.09
 // @author		Owyn
 // @contributor	ubless607, bitst0rm
 // @namespace	handyimage
@@ -910,6 +910,7 @@
 // @match		https://iimg.su/i/*
 // @match		https://radikal.cloud/i/*
 // @match		https://hamster.is/image/*
+// @match		https://hamsterimg.net/image/*
 // @match		https://coomimgs.net/image/*
 // @match		https://javring.com/upload/*
 // @match		https://xcamcovid.com/upload/*
@@ -1444,6 +1445,7 @@ function makeworld()
 	case "coomimgs.net":
 	case "ltdfoto.ru":
 	case "vgy.me":
+	case "hamsterimg.net":
 		i = document.querySelector('meta[property="og:image"], [name="og:image"]');
 		if(i)
 		{
